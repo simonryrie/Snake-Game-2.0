34e7e4a2764515b6fd298be6878ae69dbe391e3a
simonryrie <simonryrie@gmail.com>
2023-05-04 21:10:12 +0100

    Initial commit

---
+++ snake-game/src/components.ts
@@ -0,0 +1,12 @@
+export const canvas_size = { x: 800, y: 800 };
+export const snake_start = [{ x: 8, y: 7 }, { x: 8, y: 8 }];
+export const direction_start = { x: 0, y: -1 };
+export const apple_start = { x: 8, y: 3 };
+export const scale = 40;
+export const initial_speed = 200;
+export const directions = {
+  ArrowUp: { x: 0, y: -1 },
+  ArrowDown: { x: 0, y: 1 },
+  ArrowLeft: { x: -1, y: 0 },
+  ArrowRight: { x: 1, y: 0 },
+};
